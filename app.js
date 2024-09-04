26b333afbe64059a7bdc9a311218734aaa7c622b
yeo <yepeleyayeo00@gmail.com>
2024-09-04 04:41:24 +0200

    word

--- app.js
+++ app.js
@@ -69,7 +69,7 @@
         const url = URL.createObjectURL(blob);
         const a = document.createElement('a');
         a.href = url;
-        a.download = 'output.csv';
+        a.download = 'output.doc';
         document.body.appendChild(a)
         a.click();
         document.body.removeChild(a)
